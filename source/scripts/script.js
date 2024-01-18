1d419d6ada27891701d4883eb9450d7c1733c474
eduardo <marketingdoeduardo@gmail.com>
2024-01-17 22:15:49 -0300

    retirando a seção de Shows

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('butto');
     const paragrafo = document.querySelector('.paragrafo--none');
+    
 
     document.querySelector('a[href="#Trailer"]').addEventListener('click', (e) => {
         e.preventDefault()
@@ -9,21 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
             document.getElementById('Trailer').scrollIntoView({behavior: 'smooth'});;
         }, 1000)
     })
-
-    window.addEventListener('scroll', () => {
-        const shows = document.querySelector('.shows');
-        const carousel = document.querySelector('.carousel');
-        const alturaCarousel = carousel.clientHeight;
-        const posicaoAtual = window.scrollY;
-
-        if (posicaoAtual > alturaCarousel) {
-            shows.style.display = 'block';
-        }
-
-        if (posicaoAtual === 0) {
-            shows.style.display = 'none';
-        }
-    });
 
     button.addEventListener('click', () => {
         paragrafo.style.display = 'block';
